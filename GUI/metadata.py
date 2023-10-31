333f987a0197158455f5c9fe3380a8ab9c6cd96f
Mikel Val Calvo <mikel@sputnik.mv82>
2023-10-31 11:12:32 +0100

    loading data bug solved

--- GUI/metadata.py
+++ GUI/metadata.py
@@ -51,12 +51,25 @@ class MetadataManager:
     def parse_datasetPath(self, given_path: str):
         # Get the relative path from the specified directory
         relative_path = os.path.relpath(given_path, os.getcwd())
+
         # Now split the relative path into its components
         path_parts = relative_path.split(os.sep)
-        # Construct the desired outputs
-        self.base_path = os.path.join('.', path_parts[0], '')  # Adding a trailing slash
-        self.dataset_folder = path_parts[1]
-        self.dataset_file = os.path.splitext(path_parts[2])[0]  # Removing the file extension
+
+        # Handle the base path
+        if len(path_parts) > 2:
+            self.base_path = os.path.join('.', *path_parts[:-2])
+        else:
+            self.base_path = "./"
+
+        # Handle the dataset folder
+        if len(path_parts) > 2:
+            self.dataset_folder = path_parts[-2]
+        else:
+            self.dataset_folder = ""
+
+        # Handle the dataset file
+        self.dataset_file = os.path.splitext(path_parts[-1])[0]  # Removing the file extension
+
         # Output
         print(f'base_path : "{self.base_path}"')
         print(f'dataset_folder : "{self.dataset_folder}"')
