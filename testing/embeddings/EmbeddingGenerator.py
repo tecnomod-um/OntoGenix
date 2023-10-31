62d2a562646076da89a3c103d4757e100099db47
Mikel Val Calvo <mikel@sputnik.mv82>
2023-10-31 12:37:51 +0100

    create testing directory for testing ideas

---
+++ testing/embeddings/EmbeddingGenerator.py
@@ -0,0 +1,17 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.decomposition import PCA
+
+
+class EmbeddingGenerator:
+
+    def __init__(self, model='all-MiniLM-L6-v2'):
+        self.model = SentenceTransformer(model)
+
+    def get_embeddings(self, sentences):
+        embeddings = self.model.encode(sentences)
+        return embeddings
+
+    @staticmethod
+    def get_pca(embeddings, n_components=2):
+        reduced_dimensions = PCA(n_components=n_components).fit_transform(embeddings)
+        return reduced_dimensions
